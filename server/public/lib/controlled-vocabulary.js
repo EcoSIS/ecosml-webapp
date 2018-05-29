f3d727e0b895a300c932d114850cf4a3fcd5b8e9
Justin Merz <jrmerz@gmail.com>
2018-05-29 08:44:27 -0700

    adding additional theme vocabulary, issue #7

--- server/public/lib/controlled-vocabulary.js
+++ server/public/lib/controlled-vocabulary.js
@@ -10,5 +10,24 @@ module.exports = {
     LMA : [],
     photosynthesis : ['Vcmax', 'Jmax', 'Ev', 'A-Ci'],
     fluorescence : ['fPAR', 'GPP']
+  },
+  abiotic : {
+    plastics : [],
+    methane : [],
+    ice : []
+  },
+  pathogen : {
+    mold : ['late blight', 'early blight'],
+    bacteria : [],
+    virus : ['PVY'],
+    fungus : ['verticilium']
+  },
+  damage : {
+    pests : [],
+    herbicide : []
+  },
+  production : {
+    yield : [],
+    'yield quality' : []
   }
 }
